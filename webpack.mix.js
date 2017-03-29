e633535232ed0748c7d696fd7025c2a24836a8e0
Daniel <danielsp.pinto@gmail.com>
2017-03-29 00:55:51 -0300

    foi feito a instalação da dependencia do frontend Gentelella e depois as demais dependencia

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,13 @@ const { mix } = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
    .sass('resources/assets/sass/app.scss', 'public/css');
+
+mix.copy('node_modules/gentelella/vendors/bootstrap/dist/css/bootstrap.min.css', 'public/css')
+  .copy('node_modules/gentelella/vendors/font-awesome/css/font-awesome.min.css', 'public/css')
+  .copy('node_modules/gentelella/build/css/custom.min.css', 'public/css');
+
+mix.copy('node_modules/gentelella/vendors/jquery/dist/jquery.min.js', 'public/js')
+  .copy('node_modules/gentelella/vendors/bootstrap/dist/js/bootstrap.min.js', 'public/js')
+  .copy('node_modules/gentelella/vendors/fastclick/lib/fastclick.js', 'public/js')
+  .copy('node_modules/gentelella/vendors/nprogress/nprogress.js', 'public/js')
+  .copy('node_modules/gentelella/build/js/custom.min.js', 'public/js');
