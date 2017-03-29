f470af9f12218e66feee6a60179db1a3698c11dd
Daniel <danielsp.pinto@gmail.com>
2017-03-29 02:05:33 -0300

    foi implemantado o auth do Laravel e o frontend do gentelella

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -51,6 +51,7 @@ class RegisterController extends Controller
             'name' => 'required|max:255',
             'email' => 'required|email|max:255|unique:users',
             'password' => 'required|min:6|confirmed',
+            'auth' => 'required|max:1',
         ]);
     }
 
@@ -66,6 +67,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
+            'auth' => $data['auth'],
         ]);
     }
 }
